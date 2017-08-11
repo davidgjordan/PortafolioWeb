e0b3a52adacd3393da3f392d5736f82d27580af1
davidgjordan <davidgjordancbba@gmail.com>
2017-08-11 19:15:47 -0400

    Rutas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+//componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
+//rutas
+import { ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { PortafolioItemComponent } from './components/portafolio-item/portafolio
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+//importamos los componentes
+import { AboutComponent, PortafolioComponent, PortafolioItemComponent } from './components/index.paginas';
+
+//arrray con la configuracion de rutas
+
+const APP_ROUTES:Routes =[
+    { path: '',  component:PortafolioComponent },
+    { path: 'home',  component:PortafolioComponent },
+    { path: 'about',  component:AboutComponent },
+    { path: 'portafolio-item',  component:PortafolioItemComponent },
+    { path: '**', redirectTo:'', pathMatch:'full' }
+];
+
+
+export const ROUTING :ModuleWithProviders = RouterModule.forRoot(APP_ROUTES, { useHash:true });
+
